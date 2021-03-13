4b402c35cdeb2f6c99462efd779a214c50cea6f5
PandaTRX <francosteffanoanticona@gmail.com>
2021-03-13 13:19:10 -0500

    PandaTron

---
+++ README.md
@@ -0,0 +1,2 @@
+# PandaTron
+ Smart Contract
---
+++ src/components/Contract/index.js
@@ -0,0 +1,5 @@
+
+/// La direccion de su contrato acá ///////////////////////////////////
+const contractAddress = "TVukxPCjCKsk5RVE24zmYuQisyGcoWGkJi";
+
+export default contractAddress;
